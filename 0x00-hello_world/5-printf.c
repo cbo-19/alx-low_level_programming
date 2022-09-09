d733babb12ebd8c1791107911b072db7cf2ac025
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-08 17:13:41 -0700

    Starting C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+{#include<stdio.h>
 /**
  * main -Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the  outcome is a piece of art,\n");
+	printf("with proper grammar, but the  outcome is a piece of art,");
 	return (0);
+}
 }
