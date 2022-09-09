d8f694d55e6145944bf58b1b8149dfa553b60666
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-09 15:02:03 -0700

    Starting C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,21 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n= rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 
-	if(n>0)
+	if (n > 0)
 	{
-		printf("%d is positive", n);}
-	else {
-		if(n<0)
+		printf("%d is positive", n);
+	}
+	else
+	{
+		if (n < 0)
 		{
-			printf("%d is negative", n);}
-		else {
-			if(n==0)
+			printf("%d is negative", n);
+		}
+		else
+		{
+			if (n == 0)
 				printf("%d is zero", n);
 		}
 	}
