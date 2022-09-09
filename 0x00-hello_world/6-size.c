c8701444e3a131809669a66b99cecc3ced30b686
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-08 17:51:08 -0700

    Starting C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	float f;
 	long int l;
 	long long int d;
-	
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
