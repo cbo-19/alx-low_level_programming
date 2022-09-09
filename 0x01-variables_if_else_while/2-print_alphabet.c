88d07bb786eabdd3671b51c2533b971bb2903b4a
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-09 16:41:44 -0700

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*Declaring a variable c which is a character type */
+	char c;
+	/*Using for loop to start at the alphabet 'a' to 'z' */
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,5 @@ This reademe is for coding in C
 0-positive_or_negative runs a C program
 
 1-last_digit prints the last digit of an integer
+
+2-print_alphabet prints lower alphabets
