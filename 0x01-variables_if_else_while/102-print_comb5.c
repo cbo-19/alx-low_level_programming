8d4360b8bc04d00c042f838b5c6f4cf5467f91e5
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-09 19:48:11 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+	/*assigning values to l andh with for loop*/
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 100) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				/*setting limit for comma*/
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
