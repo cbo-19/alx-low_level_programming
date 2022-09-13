69e0178a2f0df9637dc01968f4ed821215caa86d
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-13 14:43:58 -0700

    function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,10 +18,13 @@ void times_table(void)
 		for (num = 0; num <= 9; num++)
 		{
 			int x = num * n;
+
 			_putchar(x + '0');
 			_putchar(',');
+			_putchar(' ');
+			_putchar('\n');
 		}
 		n++;
-		num= '0';
+		num = '0';
 	}
 }
