c068e6b85198ba2314c657ae53ce668adc05d108
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-14 02:31:19 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,12 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 0)*/
-	/*{*/
-		/*putchar(i);*/
-	/*}*/
+	/*
+	 * while (i < 0)
+	*{
+		*putchar(i);
+	*}
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
