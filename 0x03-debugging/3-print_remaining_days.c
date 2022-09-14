c5daec9182607b46ae05deb86a3a848e0198de96
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-14 04:06:17 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,30 +16,22 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-			day ++;
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			day++;
+		}
+		
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		else if (month <= 2 && day <= 60)
+		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
-	}
-	else if (day == 60 && month == 2)
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
 	}
 }
