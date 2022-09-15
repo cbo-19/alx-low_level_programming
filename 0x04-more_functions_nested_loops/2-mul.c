513ec0be00fec7593a2f842286f71c66818f54d6
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-15 12:48:31 -0700

    more

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer for program
+ * @b: integer for program
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int a = rand ();
+	int b = rand ();
+	int c = a * b;
+
+	putchar(c + '0');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,5 @@ This is a readme for coding in C
 0-isupper.c checks for uppercases
 
 1-isdigit.c checks for digits
+
+2-mul.c multiplies two integers
