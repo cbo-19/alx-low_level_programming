63def226d1a618ca9d4b7f1ff1478dd75a3ebb7d
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-15 15:59:58 -0700

    more

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/*8
+ * print_triangle - prints triangle
+ *
+ * @size: value
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size < 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				putchar ('#');
+			}
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
