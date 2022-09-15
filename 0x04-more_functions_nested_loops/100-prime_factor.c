c359430f181337d4af00d6cd9f02ab8c682ea3b5
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-15 16:10:20 -0700

    more

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * main - finds and prints largest prime number
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long prime = 612952475143, div;
+
+	while (div < (prime/2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (div = 3; div < (prime / 2); div += 2)
+		{
+			if ((prime % div) == 0)
+				prime /= div;
+		}
+	}
+	printf("%ld\n", prime);
+
+	return (0);
+}
