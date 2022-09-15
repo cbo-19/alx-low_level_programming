28f653aeed401a3ff4865fedf805d53571628bf4
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-15 13:57:35 -0700

    more

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 && i != 4)
+		if ((i != 2) && (i != 4))
 		{
-			write(1, &i, 1);
+			putchar(i + '0');
 		}
 	}
 	putchar('\n');
