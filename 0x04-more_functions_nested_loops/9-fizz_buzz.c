8ef575527229f02452f33573549f3e7feac84349
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-15 15:46:51 -0700

    more

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - fizz buzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0);
+		{
+			printf("Buzz");
+		}
+		if (n < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
