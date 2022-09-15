04d52a7e5db6d24a426968ea4a9b2256af503dce
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-15 13:04:16 -0700

    more

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
