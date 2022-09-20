1eff34ed4c385eadbe6ff647e60338ffc8bf37f2
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-19 19:27:35 -0700

    point

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copies strings
+ *
+ * @dest: the source
+ * @src: the destination
+ *
+ * Return: Dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length;
+	for (length = 0; src[length] != '\0'; length++)
+	{
+		dest[length] = src[length];
+	}
+	dest[length] = src[length];
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -19,6 +19,8 @@ void puts_half(char *str);
 
 void print_array(int *a, int n);
 
+char *_strcpy(char *dest, char *src);
+
 
 
 #endif
