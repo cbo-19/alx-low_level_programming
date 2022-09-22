3a546c4e7a580b67381f21643685d5ad4774b172
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-22 08:30:13 -0700

    point

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	int result = 0;
 	int i;
 
-	while ((s1[i] != '\0' && s2[i] !='\0') && s1[i] == s2[i])
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
 		i++;
 	}
