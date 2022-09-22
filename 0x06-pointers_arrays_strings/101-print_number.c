8be9a66db723dcd4085a4a2150e441d6816c37a6
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-09-22 20:20:50 +0000

    point

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include<stdio.h>
 /**
   * print_number - print numbers chars
   * @n: integer params
@@ -14,7 +14,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n1 = -n;
 	}
 
