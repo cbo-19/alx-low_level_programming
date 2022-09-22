fe0192a07d2804fc70f9dd322393ffdb9c197f44
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-09-22 20:01:46 +0000

    pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,16 @@
-#include<string.h>
 #include "main.h"
-
 /**
- * _strcmp - compares two strings
- * @s1: parameter pointer to char
- * @s2: parameter pointer to char
+ * _strcmp - this function compares two strings
+ *
+ * @s1: dest string
+ * @s2: src string
  *
  * Return: result
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int result = 0;
-	int i;
+	int i = 0;
 
 	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
@@ -21,8 +19,6 @@ int _strcmp(char *s1, char *s2)
 	if (s1[i] == s2[i])
 		result = 0;
 	else
-	{
 		result = s1[i] - s2[i];
-	}
 	return (result);
 }
