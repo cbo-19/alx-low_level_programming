cfbc38b07d91cc18384d307161eebc82b7757776
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-22 12:22:19 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include<string.h>
+#include "main.h"
+
+/**
+ * string_toupper - changes lower cases to upper
+ *
+ * @string: dest string
+ *
+ */
+
+char *string_toupper(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if(string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] -= 32;
+		}
+	}
+	return (string)
+}
