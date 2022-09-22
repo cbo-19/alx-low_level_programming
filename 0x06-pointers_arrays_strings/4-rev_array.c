b16a7ef0fbbafc208c6a3ef58d8ff90af1831baf
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-09-22 11:46:07 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "main.h"
+#include<string.h>
+
+/**
+ * reverse_array - reverse the content of an array
+ *
+ * @a: parameter pointer to int
+ * @n: number of byte limit
+ *
+ */
+
+void reverse_array(int *a, int n);
+{
+	int rev;
+	int i = 0;
+	int j = n - 1;
+
+	while (j <= i)
+	{
+		rev = a[j];
+		a[j] = a[i];
+		a[i] = rev;
+
+		i--;
+		j++;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,10 @@
 THIS IS A README FOR POINTERS AND ARRAYS
 0-strcat - concatenates two strings
+
+1-strncat concatenates two strings
+
+2-strncpy copies string
+
+3-strcmp
+
+4-rev_array
