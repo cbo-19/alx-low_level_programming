a0d4e0ce2bc562f6d2ed3095535a521af7fda8d6
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-10-04 13:37:27 +0000

    Update 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<ctype.h>
+#include "main.h"
+/**
+ *_islower - checks for lower cases in inputs
+ *
+ * @c: The character to print
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
