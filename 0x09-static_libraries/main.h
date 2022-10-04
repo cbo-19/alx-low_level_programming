bb38ec2aca8fc8ac30435cdc478d74989a5307c8
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-10-04 07:26:23 -0700

    static lib

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 int _islower(int c);
-int isalpha(int c);
+int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
@@ -21,3 +21,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+
+#endif
