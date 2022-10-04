7140f1050993c0036c67c4fd33721ec02485662e
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-10-04 13:50:56 +0000

    Update 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+/**
+ * _puts -  prints an output
+ *
+ * @str: character value to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
