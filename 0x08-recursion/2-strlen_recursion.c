c6691e8d17678f08ab7bfd89ff517d22f549b4fe
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-10-06 09:58:04 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+
+/**
+ *_strlen_recursion -  returns the length of string
+ *
+ * @s: character to work with.
+ *@i: a character value
+ * Returns: i
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	return (i);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,5 @@ This is my readme file for recursion.
 0-puts_recursion.c prints a string
 
 1-print_rev_recursion reverses a string using recursion
+
+2-strlen_recursion.c returns the length of a string
