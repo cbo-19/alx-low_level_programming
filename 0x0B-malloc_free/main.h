8792f30ff6b8ba4f721b3e06e0933908f5c8daea
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-10-10 09:16:42 -0700

    malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,4 @@
-*putchar function*/
+/*putchar function*/
 int _putchar(char c);
 /*function that creates an array of chars, and initializes it with a specific char*/
 char *create_array(unsigned int size, char c);
